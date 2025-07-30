7767d324010b1b3ede6f107b9d3f22be2bffaf70
Rafied02 <rafid.shidqi021996@gmail.com>
2025-07-30 18:46:21 +0700

    Fix index.js and package.json for Railway

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
-const usersRouter = require('./routes/users');
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
-// Route utama
-app.use('/users', usersRouter);
-
-// Cek server jalan
 app.get('/', (req, res) => {
   res.send('ERP Backend Running!');
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
